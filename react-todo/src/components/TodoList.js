cd7a20b89af35dd34b08c6880df4d7e65a018aef
Sam Rodriguez <samrodriguez826@gmail.com>
2022-07-17 14:54:02 -0400

    removeTodo function

--- react-todo/src/components/TodoList.js
+++ react-todo/src/components/TodoList.js
@@ -16,6 +16,12 @@ function TodoList() {
 
     }
 
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id != id)
+
+        setTodos(removeArr) 
+    }
+
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
             if(todo.id === id){
@@ -34,6 +40,7 @@ function TodoList() {
         <Todo
             todos={todos}
             completeTodo={completeTodo}
+            removeTodo={removeTodo}
          />
     </div>
   )
